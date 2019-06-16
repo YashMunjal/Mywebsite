a087cfc2780f1de38f6916256bf004118c56ae42
YashMunjal <44050159+YashMunjal@users.noreply.github.com>
2019-06-16 23:14:31 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,73 @@
+jQuery(function ($) {
+
+    // Dropdown menu
+    $(".sidebar-dropdown > a").click(function () {
+        $(".sidebar-submenu").slideUp(200);
+        if ($(this).parent().hasClass("active")) {
+            $(".sidebar-dropdown").removeClass("active");
+            $(this).parent().removeClass("active");
+        } else {
+            $(".sidebar-dropdown").removeClass("active");
+            $(this).next(".sidebar-submenu").slideDown(200);
+            $(this).parent().addClass("active");
+        }
+
+    });
+    //Pin sidebar
+        if ($(".page-wrapper").hasClass("pinned")) {
+            // unpin sidebar when hovered
+            $(".page-wrapper").removeClass("pinned");
+            $("#sidebar").unbind( "hover");
+        } else {
+            $(".page-wrapper").addClass("pinned");
+            $("#sidebar").hover(
+                function () {
+                    console.log("mouseenter");
+                    $(".page-wrapper").addClass("sidebar-hovered");
+                },
+                function () {
+                    console.log("mouseout");
+                    $(".page-wrapper").removeClass("sidebar-hovered");
+                }
+            )
+
+        };
+
+
+    //toggle sidebar overlay
+    $("#overlay").click(function () {
+        $(".page-wrapper").toggleClass("toggled");
+    });
+
+    //switch between themes 
+    var themes = "default-theme light-theme";
+    $('[data-theme]').click(function () {
+        $('[data-theme]').removeClass("selected");
+        $(this).addClass("selected");
+        $('.page-wrapper').removeClass(themes);
+        $('.page-wrapper').addClass($(this).attr('data-theme'));
+		var x=$("#theme-selection-1").attr('class');
+		if(x=='theme default-theme selected')
+		{
+			$("body").css("background-color", "black");
+		}
+		if(x!='theme default-theme selected')
+		{
+			$("body").css("background-color", "white");
+		}
+    });
+	
+	//Theme Switching ends
+	
+	
+    //custom scroll bar
+    if (!/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        $(".sidebar-content").mCustomScrollbar({
+            axis: "y",
+            autoHideScrollbar: true,
+            scrollInertia: 300
+        });
+        $(".sidebar-content").addClass("desktop");
+
+    }
+});
